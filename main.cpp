b1e48ff6418c1d653e62a00372519a61f53a0c8c
vladimir.perez <vladimir.perez@MEXWKS1506.mex.gameloft.org>
2014-08-12 17:13:17 -0700

    Inicio

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: vladimir.perez
+ *
+ * Created on August 12, 2014, 5:08 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
