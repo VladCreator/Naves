2167202bd1d59e13ebe031cf31095e551e9690df
Vladimir Casa <vlazx@yahoo.com.mx>
2014-09-07 11:36:59 -0700

    Actividad 4
    *Muy bien en la Actividad 3
    +Agrege cosas que debes de corregir.
    *Revisa el redme y el codigo.

--- source/Game.cpp
+++ source/Game.cpp
@@ -20,78 +20,73 @@ CFrame Upnave;
 CSprite Nave;
 
 struct nave{
-    int x,y;
+	int x,y;
 } minave;
-Game::Game(void)
-{
+
+Game::Game(void){
 	Estado = INICIALIZACION;
-	
+
 	minave.x = 50;
 	minave.y = 10;
 }
-Game::~Game(void)
-{
+
+Game::~Game(void){
 }
 
-void Game::Actualizado()
-{
+void Game::Actualizado(){
 	SDL_Flip(screen);
 }
-void Game::Finalizacion()
-{
+
+void Game::Finalizacion(){
 }
-void Game::MotorEstados()
-{
+
+void Game::MotorEstados(){
 	//Estado es del enum 
-    while(Estado != FINALIZACION)
+	while(Estado != FINALIZACION)//ACT4: Debe de existir un estado para esto y no el de FINALIZACION
 	{
 		switch (Estado)
-		{
-			 case JUEGO:
-				 getch(); 
-				break;
-			case INICIALIZACION:
-				Inicializacion();
-				Juego();
-				Estado = JUEGO;
-				break;
-			
+		{///ACT4:  Se recomienda que pongas en order tus estados, primero INICIALIZACION y Luego JUEGO
+		case JUEGO:
+			getch(); //ACT4: Este GETCH aqui esta sobrando.
+			break;
+		case INICIALIZACION:
+			Inicializacion();
+			Juego();//ACT4: Este juego ya no debe de ir aqui, debe de ir en su respectivo estado.
+			Estado = JUEGO;//Correcto ya que cargas todo, aqui envias al estado JUEGO
+			break;
+
 		}
 	}
-    
+
 } 
 
-void Game::Inicializacion()
-{
-		atexit(SDL_Quit);
-
-		if(SDL_Init(SDL_INIT_VIDEO) < 0)
-		{
-			printf("No se ha podido iniciar SDL: %s\n", SDL_GetError());
-			exit(1);
-		}
-
-			screen = SDL_SetVideoMode(WIDTH, HEIGHT, BPP, SDL_HWSURFACE);
-			//Cambiar Nombre de la Ventana
-			SDL_WM_SetCaption("Juego de Naves", "s");
-
-		if(screen == NULL)
-		{
-			printf("No se ha podido establecer el modo de vídeo: %s\n", SDL_GetError());
-			exit(1);
-		}
-		if(image == NULL)
-		{
-			printf("No se ha podido cargar la imagen: %s\n",SDL_GetError());
-		}		
+void Game::Inicializacion(){
+	atexit(SDL_Quit);
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("No se ha podido iniciar SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	screen = SDL_SetVideoMode(WIDTH, HEIGHT, BPP, SDL_HWSURFACE);
+	//Cambiar Nombre de la Ventana
+	SDL_WM_SetCaption("Juego de Naves", "s");
+	if(screen == NULL)
+	{
+		printf("No se ha podido establecer el modo de vídeo: %s\n", SDL_GetError());
+		exit(1);
+	}
+	if(image == NULL)//ACT4: ¨imagge¨Se necesita?
+	{
+		printf("No se ha podido cargar la imagen: %s\n",SDL_GetError());
+	}		
 }
-void Game::Juego()
-{
-	minave.x = 50;
+
+void Game::Juego(){
+	minave.x = 50;//ACT4: Esto no debe de ir aqui, estas forzando siempre a esta posicion. En el constructor ya lo tienes.
 	minave.y = 10;
 	Nave.setx(minave.x);
 		Nave.sety(minave.y);
-		Upnave.load("Nave.bmp");
+		Upnave.load("Nave.bmp");//ACT4: En el Juego ya no se debe de cargar cosas, para eso esta el estado de INICIALIZACION
 		Nave.addframe(Upnave);
 		Nave.draw(screen);
 		SDL_Flip(screen);
--- source/Game.h
+++ source/Game.h
@@ -5,15 +5,21 @@
 
 class Game
 {
-	 enum ESTADO { INICIALIZACION,JUEGO,FINALIZACION };
-	 enum GRAFICA { PINTANDO,ACTUALIZANDO };
+	 enum ESTADO { ///ACT4: Es importante indicar de quien es el estado por ejemplo: ESTADO_JUEGO.
+		 INICIALIZACION,
+		 JUEGO,
+		 FINALIZACION };//ACT4: A diferencia de lo que puedes creer FINALIZACION debe ser un estado accesible en el Loop, aqui vamos a matar todos los objetos creados.
+
+	 enum GRAFICA { //ACT4: Tambien aqui, es importante indicar que es un estado SUBESTADO__JUEGO
+		 PINTANDO,
+		 ACTUALIZANDO };
 public:
 	Game(void);
 	~Game(void);
 
 
-	 ESTADO Estado;
-	GRAFICA Grafica;
+	 ESTADO Estado;//Bien
+	GRAFICA Grafica;//ACT4: MM.. esta nombre no me convence mucho.
 
 	void MotorEstados();
 	 void Inicializacion();
