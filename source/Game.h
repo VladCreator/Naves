426b78a744a4b192627b9fb3fa75f3b281fb9edd
lenin <lenin_66@hotmail.com>
2014-09-24 23:30:20 -0700

    Correcciones Actividad 5
    Funciona mover el objeto

--- source/Game.h
+++ source/Game.h
@@ -1,40 +1,35 @@
 #include <SDL.h>
 #pragma once
-
-
-
 class Game
 {
-	 enum ESTADO_JUEGO { ///ACT4: Es importante indicar de quien es el estado por ejemplo: ESTADO_JUEGO.//Corregido
-		 ///ACT5: Aqui tambien me equivoque, debe ser "Estado" ën vez de "ESTADO_JUEGO"
-		 INICIALIZACION,
-		 JUEGO,
-		 FINALIZACION, 
-		 SALIR_MOTOR};//ACT4: A diferencia de lo que puedes creer FINALIZACION debe ser un estado accesible en el Loop, aqui vamos a matar todos los objetos creados.
-		 //ACT5: No es necesario que le pongas MOTOR, pues al estar diciendo SALIR y al estar en tu clase Game, es el estado del GAME
-		 
-	 enum SUBESTADO_JUEGO { //ACT4: Tambien aqui, es importante indicar que es un estado SUBESTADO__JUEGO //Corregido
-		 PINTANDO,//Act5: Primero es Actualizar y luego Pintar.
+
+	 enum ESTADO { 
+		 ESTADO_INICIALIZACION,
+		 ESTADO_JUEGO,
+		 ESTADO_FINALIZACION, 
+		 ESTADO_TERMINADO};
+
+
+	 enum SUBESTADO_JUEGO {
 		 ACTUALIZANDO,
-		SALIR};//Act5: No se necesita salir.
+		 PINTANDO,
+		SALIR};
+
 public:
 	Game(void);
 	~Game(void);
 
 
-	 ESTADO_JUEGO Estado;//Bien
-	SUBESTADO_JUEGO Sub_Estado;//ACT4: MM.. esta nombre no me convence mucho. //Corregido a medias
+	ESTADO Estado;//Bien
+	SUBESTADO_JUEGO Sub_Estado;
 
 	void MotorEstados();
 	 void Inicializacion();
 	 void Juego();
+	 void Terminado();
 	 void Finalizacion();
 	 void Pintado();
 	 void Actualizado();
 	 
-	 void SetEstado(ESTADO_JUEGO n);
-	 int getEstado();
-
-	 int getFINALIZACION();
 };
 
